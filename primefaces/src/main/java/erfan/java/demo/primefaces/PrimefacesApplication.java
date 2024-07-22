c251d050588baf5727226cc90a67e1340d5e045d
unknown <erfanscott@gmail.com>
2024-07-22 09:23:50 +0330

    show the primefaces demo can integrate RestControllers

--- primefaces/src/main/java/erfan/java/demo/primefaces/PrimefacesApplication.java
+++ primefaces/src/main/java/erfan/java/demo/primefaces/PrimefacesApplication.java
@@ -1,13 +1,11 @@
 package erfan.java.demo.primefaces;
 
-// import javax.faces.webapp.FacesServlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
-import jakarta.faces.webapp.FacesServlet;
+
+
 
 @SpringBootApplication
 public class PrimefacesApplication {
