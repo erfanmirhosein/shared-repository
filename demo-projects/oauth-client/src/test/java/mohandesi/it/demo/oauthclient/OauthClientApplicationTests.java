449440b8dc95391d45223a36c6199ab79383af71
erfanscott <erfanscott@gmail.com>
2024-07-29 09:53:49 +0330

    create a springboot project to develop your oauth clients

---
+++ demo-projects/oauth-client/src/test/java/mohandesi/it/demo/oauthclient/OauthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package mohandesi.it.demo.oauthclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
