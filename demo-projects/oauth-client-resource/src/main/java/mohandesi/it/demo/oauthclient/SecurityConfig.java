0da3aab0b81901f361d0aab841c5c9d6fd460258
erfanscott <erfanscott@gmail.com>
2024-08-05 18:33:30 +0330

    provide an introspector to the resource server so it sends the url requested along with the introspect request

--- demo-projects/oauth-client-resource/src/main/java/mohandesi/it/demo/oauthclient/SecurityConfig.java
+++ demo-projects/oauth-client-resource/src/main/java/mohandesi/it/demo/oauthclient/SecurityConfig.java
@@ -1,113 +1,97 @@
 package mohandesi.it.demo.oauthclient;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.net.URI;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.log.LogMessage;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtClaimNames;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.introspection.NimbusOpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, HttpServletRequest httpServletRequest) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/user/**").hasRole("USER")
                         .anyRequest().authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2ResourceServerCustomizer -> oauth2ResourceServerCustomizer
+                        .opaqueToken(customizer -> customizer.introspector(introspector(httpServletRequest))));
+
         return http.build();
     }
 
     @Bean
-    public JwtDecoder jwtDecoder() {
+    public OpaqueTokenIntrospector introspector(HttpServletRequest httpServletRequest) {
 
-        // NimbusJwtDecoder jwtDecoder =
-        // NimbusJwtDecoder.withIssuerLocation("http://localhost:8080").build();
+        String introspectionUri = "http://localhost:8080/oauth2/introspect";
+        String clientId = "client1_id";
+        String clientSecret = "client1_secret";
+        NimbusOpaqueTokenIntrospector withAuthoritiesIntrospector = new NimbusOpaqueTokenIntrospector(introspectionUri,
+                clientId, clientSecret);
+        withAuthoritiesIntrospector
+                .setRequestEntityConverter(
+                        new WithAuthoritiesRequestEntityConverter(URI.create(introspectionUri), httpServletRequest,
+                                clientId, clientSecret));
 
-        // Converter<Jwt, Collection<GrantedAuthority>> realmAccessConverter = new
-        // JwtConverterIncludingAuthorities();
-
-        // Map<String, Converter<Jwt, Collection<GrantedAuthority>>> claimTypeConverters
-        // = new HashMap<>();
-
-        // claimTypeConverters.put("realm-access", realmAccessConverter);
-
-        // MappedJwtClaimSetConverter converter =
-        // MappedJwtClaimSetConverter.withDefaults(
-        // Collections.singletonMap("realm-access", claimTypeConverters));
-
-        // jwtDecoder.setClaimSetConverter(converter);
-
-        // return jwtDecoder;
-
-        return JwtDecoders.fromIssuerLocation("http://localhost:8080");
-
+        return withAuthoritiesIntrospector;
     }
 
-    /*
-     * Another Way to change the Behavior of the JWTConverter
-     */
+    private static class WithAuthoritiesRequestEntityConverter
+            implements Converter<String, RequestEntity<?>> {
 
-    @Bean
-    public JwtAuthenticationConverter jwtConverterGrantedAuthorities() {
+        private URI introspectionUri;
+        private HttpServletRequest httpServletRequest;
+        private String clientId;
+        private String clientSecret;
 
-        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(new JwtConverterIncludingAuthorities());
-        return converter;
-
-    }
-
-    public final class JwtConverterIncludingAuthorities implements Converter<Jwt, Collection<GrantedAuthority>> {
+        WithAuthoritiesRequestEntityConverter(URI introspectionUri,
+                HttpServletRequest httpServletRequest, String clientId, String clientSecret) {
+            this.introspectionUri = introspectionUri;
+            this.httpServletRequest = httpServletRequest;
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+        }
 
         @Override
-        public Collection<GrantedAuthority> convert(Jwt jwt) {
-            Collection<String> realmAccess = getAuthorities(jwt);
+        public RequestEntity<MultiValueMap<String, String>> convert(String token) {
 
-            for (String string : realmAccess) {
-                System.out.println(string);
-            }
+            HttpHeaders headers = requestHeaders();
 
-            return realmAccess.stream()
-                    .map(grantedAuthority -> new SimpleGrantedAuthority(grantedAuthority))
-                    .collect(Collectors.toList());
+            MultiValueMap<String, String> body = requestBody(token);
+
+            return new RequestEntity<>(body, headers, HttpMethod.POST,
+                    this.introspectionUri);
+
         }
 
-        private Collection<String> getAuthorities(Jwt jwt) {
-            String claimName = "realm-access";
-
-            Object realmAccess = jwt.getClaim(claimName);
-
-            // @SuppressWarnings("unchecked")
-            if (realmAccess instanceof Collection) {
-                return (Collection<String>) realmAccess;
-            }
-
-            return Collections.emptySet();
+        private HttpHeaders requestHeaders() {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            headers.setBasicAuth(this.clientId, this.clientSecret);
+            return headers;
         }
+
+        private MultiValueMap<String, String> requestBody(String token) {
+            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+            body.add("token", token);
+            body.add("realm-access", httpServletRequest.getServletPath());
+            return body;
+        }
+
     }
 
 }
